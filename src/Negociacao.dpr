ce6ee3de2b8ee2e3db05e90a0db52947888f72d4
Rodrigo Nazareth <rodrigonazareth.analista@gmail.com>
2020-04-10 11:24:14 -0300

    ...

--- src/Negociacao.dpr
+++ src/Negociacao.dpr
@@ -5,7 +5,17 @@ uses
   Principal in 'Telas\Principal.pas' {FormPrincipal},
   _ConexaoBanco in 'Biblioteca\_ConexaoBanco.pas',
   Ambiente in 'Biblioteca\Ambiente.pas',
-  Biblioteca in 'Biblioteca\Biblioteca.pas';
+  Biblioteca in 'Biblioteca\Biblioteca.pas',
+  Cadastro in 'Telas\Cadastro.pas' {FormCadastro},
+  CadastroProdutor in 'Telas\CadastroProdutor.pas' {FormCadastroProdutor},
+  CadastroDistribuidor in 'Telas\CadastroDistribuidor.pas' {FormCadastroDistribuidor},
+  CadastroNegociacao in 'Telas\CadastroNegociacao.pas' {FormCadastroNegociacao},
+  CadastroProduto in 'Telas\CadastroProduto.pas' {FormCadastroProduto},
+  PesquisaNegociacao in 'Telas\PesquisaNegociacao.pas' {FormPesquisaNegociacao},
+  PesquisaProduto in 'Telas\PesquisaProduto.pas' {FormPesquisaProduto},
+  PesquisaPessoa in 'Telas\PesquisaPessoa.pas' {FormPesquisaPessoa},
+  RelacaoNegociacoes in 'Telas\RelacaoNegociacoes.pas' {FormRelacaoNegociacoes},
+  ManutencaoNegociacao in 'Telas\ManutencaoNegociacao.pas' {FormManutencaoNegociacao};
 
 {$R *.RES}
 
