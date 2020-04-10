76e7ebb86f2bde9f1c7248fa59c02b5f5e3d2d23
Rodrigo Nazareth <rodrigonazareth.analista@gmail.com>
2020-04-09 23:30:01 -0300

    telas iniciais

--- src/Negociacao.dpr
+++ src/Negociacao.dpr
@@ -2,12 +2,18 @@ program Negociacao;
 
 uses
   Forms,
-  HOME in 'HOME.PAS' {MainForm};
+  Principal in 'Telas\Principal.pas' {FormPrincipal},
+  _ConexaoBanco in 'Biblioteca\_ConexaoBanco.pas',
+  Ambiente in 'Biblioteca\Ambiente.pas',
+  Biblioteca in 'Biblioteca\Biblioteca.pas';
 
 {$R *.RES}
 
 begin
+//  Ambiente.IniciarAplicacao;
+
   Application.Initialize;
-  Application.CreateForm(TMainForm, MainForm);
+  Application.MainFormOnTaskBar := True;
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
   Application.Run;
 end.
